7f60b8d732df3efcd0e631d4f0c0ca0711064c2d
Moses Chawawa <moseschawawa@gmail.com>
2021-07-29 14:17:42 +0200

    Added more tests

--- training_subject_validation/form_validators/education_questionnaire_validator.py
+++ training_subject_validation/form_validators/education_questionnaire_validator.py
@@ -5,11 +5,6 @@ from edc_form_validators import FormValidator
 class EducationQuestionnaireFormValidator(FormValidator):
 
     def clean(self):
-        # self.required_if(
-        #     OTHER,
-        #     field='previous_work',
-        #     field_required='previous_work_other'
-        # )
         self.required_if(
             YES,
             field='working_status',
--- training_subject_validation/tests/test_education_questionnaire_validator.py
+++ training_subject_validation/tests/test_education_questionnaire_validator.py
@@ -1,1 +1,35 @@
+import os
 
+from django.core.exceptions import ValidationError
+import django
+from django.test import TestCase
+from edc_constants.constants import NO, OTHER, YES
+
+from ..form_validators.education_questionnaire_validator import EducationQuestionnaireFormValidator
+
+
+class EducationQuestionValidatorTests(TestCase):
+
+    def test_type_of_work_required(self):
+        data = {
+            'working_status': YES
+        }
+        form = EducationQuestionnaireFormValidator(cleaned_data=data)
+        self.assertRaises(ValidationError, form.validate)
+        self.assertIn('type_of_work', form._errors)
+
+    def test_type_of_work_other_required(self):
+        data = {
+            'type_of_work': OTHER
+        }
+        form = EducationQuestionnaireFormValidator(cleaned_data=data)
+        self.assertRaises(ValidationError, form.validate)
+        self.assertIn('type_of_work_other', form._errors)
+
+    def test_previous_work_other_required(self):
+        data = {
+            'previous_work': OTHER
+        }
+        form = EducationQuestionnaireFormValidator(cleaned_data=data)
+        self.assertRaises(ValidationError, form.validate)
+        self.assertIn('previous_work_other', form._errors)
